50e9c0618653c80b6c07380294ea3bb6553d3733
jerrytc7 <jerrykondner@gmail.com>
2020-12-20 15:27:30 -0500

    commit tree 5f704f4615d0fdb34be75aae81d7f95743251c64

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each (words)
+  # put argument(s) here
+  # code here	  
+  i = 0  
+  if block_given?
+    i=0 
+    while i < words.length 
+    yield words[i]
+    times_called = N
+    i = i+1  
+    end 
+  end 
+  words 
+
+  end 
+
+
+
+my_each(["arel", "jon", "logan", "spencer"]) do |name|
+  name
+  N = name.length 
+ end 
